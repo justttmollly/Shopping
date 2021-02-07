842411d59c26a4b62c86118a6da77231b7d9e472
justttmollly <mmalikaa07@gmail.com>
2021-02-07 04:49:24 -0800

    ass3

--- Shopping/ViewController.swift
+++ Shopping/ViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var Table: UIView!
+    @IBOutlet weak var products: UITableView!
+   
+    
+    class TableViewCell : UITableViewCell{
+        @IBOutlet weak var imageCell : UIImageView!
+        @IBOutlet weak var name: UILabel!
+        @IBOutlet weak var price: UILabel!
+        
+        @IBOutlet weak var addtocart: UIButton!
+    
+        
+    }
+    
+    
+    @IBOutlet weak var Collection: UIView!
+    
+    @IBAction func segment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+                  Table.alpha = 1
+                  Collection.alpha = 0
+              } else {
+                  Table.alpha = 0
+                  Collection.alpha = 1
+              }
+    }
+    var segmentControll = UISegmentedControl()
+    @IBOutlet weak var cart: UIButton!
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        
+        
     }
 
 
